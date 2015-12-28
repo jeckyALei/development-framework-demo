278ab817670298a5e897026437892deea1d01048
jeckyzileixu <jecky.zilei.xu@embracesource.com>
2015-12-28 15:26:59 +0800

    test

--- spring-boot-demo/src/main/java/com/mwh/springboot/boot/Application.java
+++ spring-boot-demo/src/main/java/com/mwh/springboot/boot/Application.java
@@ -4,14 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @EnableAutoConfiguration
+// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @ImportResource("classpath*:spring/application.xml")
 public class Application {
 	private static Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		log.info("Start Server Successfully!");
