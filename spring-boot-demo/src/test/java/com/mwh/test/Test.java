32b516f1b68c316196a70eae2ad91b3db28bde0b
jeckyzileixu <jecky.zilei.xu@embracesource.com>
2015-12-28 15:28:14 +0800

    test

---
+++ spring-boot-demo/src/test/java/com/mwh/test/Test.java
@@ -0,0 +1,11 @@
+package com.mwh.test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println(TimeUnit.MINUTES.toSeconds(30));
+	}
+
+}
